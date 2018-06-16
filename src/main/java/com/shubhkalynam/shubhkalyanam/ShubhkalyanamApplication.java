e9d4c1635f851228830f2402e536af60a462339a
R Praveen Kumar <transduce0622@gmail.com>
2018-06-16 12:50:33 +0530

    First Commit for ShubhKalyanam

---
+++ src/main/java/com/shubhkalynam/shubhkalyanam/ShubhkalyanamApplication.java
@@ -0,0 +1,12 @@
+package com.shubhkalynam.shubhkalyanam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShubhkalyanamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShubhkalyanamApplication.class, args);
+	}
+}
